a2af2eca69bd20d2259fb39d742d0e5beee45d89
Varun Talwar <varun.talwar@sambanovasystems.com>
2021-08-09 18:58:00 -0700

    remaking gpt

--- example.py
+++ example.py
@@ -1,5 +1,17 @@
-from transformers import GPT2Config, GPT2Tokenizer, GPT2ForSequenceClassification
-# import torch
+import io
+import os
+import torch
+from tqdm import tqdm
+from torch.utils.data import Dataset, DataLoader
+from sklearn.metrics import classification_report, accuracy_score
+from transformers import (set_seed,
+                          TrainingArguments,
+                          Trainer,
+                          GPT2Config,
+                          GPT2Tokenizer,
+                          AdamW, 
+                          get_linear_schedule_with_warmup,
+                          GPT2ForSequenceClassification)
 
 # tokenizer = GPT2Tokenizer.from_pretrained('microsoft/DialogRPT-updown')
 # model = GPT2ForSequenceClassification.from_pretrained('microsoft/DialogRPT-updown')
@@ -10,29 +22,159 @@ from transformers import GPT2Config, GPT2Tokenizer, GPT2ForSequenceClassificatio
 # loss = outputs.loss
 # logits = outputs.logits
 
-# Get model configuration.
-print('Loading configuraiton...')
-model_config = GPT2Config.from_pretrained(pretrained_model_name_or_path=model_name_or_path, num_labels=n_labels)
+class Gpt2ClassificationCollator(object):
+    r"""
+    Data Collator used for GPT2 in a classificaiton rask. 
+    
+    It uses a given tokenizer and label encoder to convert any text and labels to numbers that 
+    can go straight into a GPT2 model.
 
-# Get model's tokenizer.
-print('Loading tokenizer...')
-tokenizer = GPT2Tokenizer.from_pretrained(pretrained_model_name_or_path=model_name_or_path)
-# default to left padding
-tokenizer.padding_side = "left"
-# Define PAD Token = EOS Token = 50256
-tokenizer.pad_token = tokenizer.eos_token
+    This class is built with reusability in mind: it can be used as is as long
+    as the `dataloader` outputs a batch in dictionary format that can be passed 
+    straight into the model - `model(**batch)`.
+
+    Arguments:
+
+      use_tokenizer (:obj:`transformers.tokenization_?`):
+          Transformer type tokenizer used to process raw text into numbers.
+
+      labels_ids (:obj:`dict`):
+          Dictionary to encode any labels names into numbers. Keys map to 
+          labels names and Values map to number associated to those labels.
+
+      max_sequence_len (:obj:`int`, `optional`)
+          Value to indicate the maximum desired sequence to truncate or pad text
+          sequences. If no value is passed it will used maximum sequence size
+          supported by the tokenizer and model.
+
+    """
+
+    def __init__(self, use_tokenizer, labels_encoder, max_sequence_len=None):
+        # Tokenizer to be used inside the class.
+        self.use_tokenizer = use_tokenizer
+        # Check max sequence length.
+        self.max_sequence_len = use_tokenizer.model_max_length if max_sequence_len is None else max_sequence_len
+        # Label encoder used inside the class.
+        self.labels_encoder = labels_encoder
+
+    def __call__(self, sequences):
+        r"""
+        This function allowes the class objesct to be used as a function call.
+        Sine the PyTorch DataLoader needs a collator function, I can use this 
+        class as a function.
+
+        Arguments:
+
+          item (:obj:`list`):
+              List of texts and labels.
+
+        Returns:
+          :obj:`Dict[str, object]`: Dictionary of inputs that feed into the model.
+          It holddes the statement `model(**Returned Dictionary)`.
+        """
+
+        # Get all texts from sequences list.
+        texts = [sequence['text'] for sequence in sequences]
+        # Get all labels from sequences list.
+        labels = [sequence['label'] for sequence in sequences]
+        # Encode all labels using label encoder.
+        labels = [self.labels_encoder[label] for label in labels]
+        # Call tokenizer on all texts to convert into tensors of numbers with 
+        # appropriate padding.
+        inputs = self.use_tokenizer(text=texts, return_tensors="pt", padding=True, truncation=True,  max_length=self.max_sequence_len)
+        # Update the inputs with the associated encoded labels as tensor.
+        inputs.update({'labels':torch.tensor(labels)})
+
+        return inputs
+
+class FinanceDataset(Dataset):
+    r"""
+    PyTorch Dataset class for loading data.
+    This is where the data parsing happens.
+    This class is built with reusability in mind: it can be used as is as.
+    Arguments:
+    path (:obj:`str`):
+        Path to the data partition.
+    """
+
+    def __init__(self, data_dir, phase):
+        self.texts = []
+        self.labels = []
+        self._create_examples(self._read_tsv(os.path.join(data_dir, (phase + ".csv"))), phase)
+        # Number of exmaples.
+        self.n_examples = len(self.labels)
+    def __len__(self):
+        r"""
+        When used `len` return the number of examples.
+        """
+        return self.n_examples
+    def __getitem__(self, item):
+        r"""
+        Given an index return an example from the position.
+        Arguments:
+        item (:obj:`int`):
+            Index position to pick an example to return.
+        Returns:
+            :obj:`Dict[str, str]`: Dictionary of inputs that contain text and asociated labels.
+        """
+        return {
+            'text':self.texts[item],
+            'label':self.labels[item]
+        }
+    def _read_tsv(self, input_file):
+        r"""Reads a tab separated value file."""
+        with open(input_file, "r") as f:
+            reader = csv.reader(f, delimiter="\t")
+            lines = []
+            for line in reader:
+                if sys.version_info[0] == 2:
+                    line = list(unicode(cell, 'utf-8') for cell in line)
+                lines.append(line)
+        return lines
+    def _create_examples(self, lines, set_type):
+        """Creates examples for the training and dev sets."""
+        examples = []
+        for (i, line) in enumerate(lines):
+            if i == 0:
+                continue
+            guid = "%s-%s" % (set_type, str(i))
+            text = line[1]
+            label = line[2]
+            texts.append(text)
+            labels.append(label)
+
+def main():
+    """
+    # Get model configuration.
+    print('Loading configuraiton...')
+    model_config = GPT2Config.from_pretrained(pretrained_model_name_or_path=model_name_or_path, num_labels=n_labels)
+
+    # Get model's tokenizer.
+    print('Loading tokenizer...')
+    tokenizer = GPT2Tokenizer.from_pretrained(pretrained_model_name_or_path=model_name_or_path)
+    # default to left padding
+    tokenizer.padding_side = "left"
+    # Define PAD Token = EOS Token = 50256
+    tokenizer.pad_token = tokenizer.eos_token
 
 
-# Get the actual model.
-print('Loading model...')
-model = GPT2ForSequenceClassification.from_pretrained(pretrained_model_name_or_path=model_name_or_path, config=model_config)
+    # Get the actual model.
+    print('Loading model...')
+    model = GPT2ForSequenceClassification.from_pretrained(pretrained_model_name_or_path=model_name_or_path, config=model_config)
 
-# resize model embedding to match new tokenizer
-model.resize_token_embeddings(len(tokenizer))
+    # resize model embedding to match new tokenizer
+    model.resize_token_embeddings(len(tokenizer))
 
-# fix model padding token id
-model.config.pad_token_id = model.config.eos_token_id
+    # fix model padding token id
+    model.config.pad_token_id = model.config.eos_token_id
 
-# Load model to defined device.
-model.to(device)
-print('Model loaded to `%s`'%device)
+    # Load model to defined device.
+    model.to(device)
+    print('Model loaded to `%s`'%device)
+    """
+    train_dataset = FinanceDataset(data_dir="/home/ubuntu/finBERT/datasets", phase="train")
+    validation_dataset = FinanceDataset(data_dir="/home/ubuntu/finBERT/datasets", phase="validation")
+    test_dataset = FinanceDataset(data_dir="/home/ubuntu/finBERT/datasets", phase="test")
+
+if __name__ == "__main__":
+    main()
